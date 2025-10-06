c85fa975d4e7e69ebc6821e8cd160012b74ece8e
getsai18 <20243ds167@utez.edu.mx>
2025-10-06 08:41:20 -0600

    ejemplo con metodo generico

--- src/classTow/App.java
+++ src/classTow/App.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+    public static <T> void printArray(T[] array){
+        for(T v : array){
+            System.out.println(v);
+        }
+    }
+
+
     public static void main(String[] args) {
         Box<String> box1 = new Box<>("Esta es una caja de Strings");
         List<String> lista = new ArrayList<>();
@@ -15,5 +22,19 @@ public class App {
         Pair<String, Integer> pair = new Pair<>("id",3);
         System.out.println("La key es: "+pair.getKey());
         System.out.println("Valor "+pair.getKey());
+
+
+        String[] arrayString={"Hola1","Hola2","Hola3"};
+        Integer[] arrayInt={1,2,3,4,5};
+
+        printArray(arrayString);
+        printArray(arrayInt);
+        System.out.println("***********************************************************************");
+        Student[] students = new Student[2];
+        Student st1=new Student(1,"getsai");
+        Student st2=new Student(2,"Juan");
+        students[0] = st1;
+        students[1] = st2;
+        printArray(students);
     }
 }
